5b20a068afa4c5c985c5c7ea945cec66564da23a
izazrizqullah <izazrizqullah@gmail.com>
2023-11-11 13:55:29 +0700

    update: testing masih gagal

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const cors = require("cors");
 const yaml = require("yamljs");
 const swaggerUI = require("swagger-ui-express");
 const apiDocumentation = yaml.load("./api-docs.yaml");
+const fs = require("fs");
+const tf = require("@tensorflow/tfjs");
 // const routes = require("./routes");
 const { hostname } = require("os");
 const app = express();
 const { PORT } = process.env;
+
+// const https = require("https");
+
+// const options = {
+//   key: fs.readFileSync("key.pem"),
+//   cert: fs.readFileSync("cert.pem"),
+//   requestCert: false,
+//   rejectUnauthorized: false,
+// };
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -19,7 +32,29 @@ app.use(morgan("dev"));
 // app.use(routes);
 
 app.get("/", (req, res) => {
-  return res.send("Welcome to -- ITINERARIO API");
+  return res.send("Welcome to -- ITINERGO API");
+});
+
+app.post("/predict", async (req, res, next) => {
+  try {
+    // Load tf model
+
+    // const modelPath = "";
+    const model = await tf.loadLayersModel(
+      "C://MyProject/Projects/ItinergoAPI/utils/models/hello.h5"
+    );
+
+    // get input data from the request
+    const { inputX } = req.body;
+
+    // Make predictions using the loaded model
+    const predictions = model.predictions(tf.tensor(inputX));
+
+    // convert predictions to json format and send the response
+    res.json({ predictions: predictions.arraySync() });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // handle route statis
@@ -45,3 +80,8 @@ app.use((err, _req, res) => {
 app.listen(PORT, () => {
   return console.log(`running on http://localhost:${PORT}`);
 });
+
+// const server = https.createServer(options, app);
+// server.listen(PORT, () => {
+//   return console.log(`running on https://localhost:${PORT}`);
+// });
