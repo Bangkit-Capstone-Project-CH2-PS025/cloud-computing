9ac3f816ae114f7f23f6ac68cd1d10bce4541d41
izazrizqullah <izazrizqullah@gmail.com>
2023-11-22 10:43:41 +0700

    update: login

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const cauth = require("../controllers/auth");
 
 router.post("/register", cauth.register);
+router.post("/login", cauth.login);
 router.put("/verify-account", cauth.activation);
 
 module.exports = router;
