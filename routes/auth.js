63d59be90f7fc0d52738eaa26afbb645f35095d1
izazrizqullah <izazrizqullah@gmail.com>
2023-12-14 06:57:44 +0700

    update: test timeout

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const cauth = require("../controllers/auth");
+const timeout = require("connect-timeout");
 
-router.post("/register", cauth.register);
-router.post("/login", cauth.login);
-router.post("/forgot-password", cauth.resetpw);
-router.get("/verify-account", cauth.activation);
+router.post("/register", timeout("20s"), cauth.register);
+router.post("/login", timeout("20s"), cauth.login);
+router.post("/forgot-password", timeout("20s"), cauth.resetpw);
+router.get("/verify-account", timeout("20s"), cauth.activation);
 router.post(
   "/reset-password/:email_token",
+  timeout("20s"),
   cauth.createNewPassword.resetPassword
 );
 router.get(
   "/reset-password/:email_token",
+  timeout("20s"),
   cauth.createNewPassword.getResetPassword
 );
 
